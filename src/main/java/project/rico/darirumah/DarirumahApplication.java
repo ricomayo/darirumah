d28093a6c77b325736f82c2171e6ba585a450bb4
Rico Mayo <idcb.riefkar@xl.co.id>
2022-11-20 16:21:14 +0700

    Initial commit

---
+++ src/main/java/project/rico/darirumah/DarirumahApplication.java
@@ -0,0 +1,13 @@
+package com.belajar.employee_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarirumahApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DarirumahApplication.class, args);
+	}
+
+}
